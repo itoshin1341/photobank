f5c2a27604fb48a3a7db6dc1aa5e21e8bdb27df2
itoshin1341 <82028845+itoshin1341@users.noreply.github.com>
2021-06-06 14:56:45 +0900

    Modify model validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,37 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  with_options presence: { message: "必須入力の項目です。"} do
+    validates :nickname
+    validates :city
+    validates :gender_id
+    validates :birthday
+    validates :introduction
+  end
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "全角（ひらがな、カタカナ、漢字）での入力をお願いします。" } do
+    validates :last_name
+    validates :first_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "全角カタカナでの入力をお願いします。" } do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+
+  with_options presence: true, numericality: { other_than: 1 } do
+    validates :prefecture_id
+  end
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: "パスワードは、半角英数字混合かつ6文字以上にて入力してください。" 
+
+  has_one_attached :image
+  # has_many :tweets
+  # has_many :comments
+
 end
