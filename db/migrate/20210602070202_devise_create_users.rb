aa0ce9e3f624f6a440c1b84017047434048f9da8
itoshin1341 <82028845+itoshin1341@users.noreply.github.com>
2021-06-06 14:52:49 +0900

    User management functions

--- db/migrate/20210602070202_devise_create_users.rb
+++ db/migrate/20210602070202_devise_create_users.rb
@@ -4,8 +4,18 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
+      t.string :nickname,           null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.integer :prefecture_id,     null: false
+      t.string :city,               null: false
+      t.string :last_name,          null: false
+      t.string :first_name,         null: false
+      t.string :last_name_kana,     null: false
+      t.string :first_name_kana,    null: false
+      t.string :gender,             null: false
+      t.date :birthday,             null: false
+      t.text :introduction,         null: false
 
       ## Recoverable
       t.string   :reset_password_token
