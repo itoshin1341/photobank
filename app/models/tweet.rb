a76a3ffde63718098bc0e6b2c3daf16812df6255
itoshin1341 <82028845+itoshin1341@users.noreply.github.com>
2021-06-09 15:19:32 +0900

    Others

---
+++ app/models/tweet.rb
@@ -0,0 +1,9 @@
+class Tweet < ApplicationRecord
+
+  # validates :prefecture_id, prefecture: true
+  # validates :city, prefecture: true
+  # validates :title, prefecture: true
+  # validates :description, prefecture: true
+  # validates :image, presence: true
+
+end
