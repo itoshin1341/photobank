5e795c919904a5d8ad20bd79be9ef65b6396165b
itoshin1341 <82028845+itoshin1341@users.noreply.github.com>
2021-07-07 15:32:53 +0900

    コントローラー（userのupdate）追記

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,26 @@
 class UsersController < ApplicationController
 
+  
   def show
     @user = User.find(params[:id])
   end
-
+  
   def edit
     @user = User.find(params[:id])
-end
-
-private
-  def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation, :introduce, :image )
   end
-
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      bypass_sign_in(@user) 
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+  
+  private
+  def user_params
+    params.require(:user).permit(:nickname, :email, :password, :password_confirmation, :prefecture_id, :city, :last_name, :first_name, :last_name_kana, :first_name_kana, :gender_id, :birthday, :introduction, :image )
+  end
 end
