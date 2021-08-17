77143dd38f9c11ddc76e141e03140b5ca65a940b
itoshin1341 <82028845+itoshin1341@users.noreply.github.com>
2021-08-17 15:33:31 +0900

    commit

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -61,8 +61,3 @@
 #   }
 
 server '18.180.124.193', user: 'ec2-user', roles: %w{app db web}
-
-set :rails_env, "production"
-set :unicorn_rack_env, "production"
-
-set :ssh_options, keys: '~/.ssh/itoshin.pem'
